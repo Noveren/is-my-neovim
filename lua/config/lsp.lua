f6c93d12e8c1a32940dcc86a27d887821382a386
Noveren <1079148276@qq.com>
2026-03-03 15:07:00 +0800

    dev: add lsp TODO

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,8 +1,11 @@
-vim.lsp.enable "lua_ls"
-vim.lsp.enable "clangd"
-vim.lsp.enable "zls"
-vim.lsp.enable "tombi"
-vim.lsp.enable "ty"
+vim.lsp.enable "lua_ls" -- lua
+vim.lsp.enable "clangd" -- c/c++
+vim.lsp.enable "zls"    -- zig
+vim.lsp.enable "tombi"  -- toml
+vim.lsp.enable "ty"     -- python
+-- TODO just-lsp https://github.com/terror/just-lsp
+-- TODO asm-lsp https://github.com/bergercookie/asm-lsp
+-- TODO https://github.com/typescript-language-server/typescript-language-server
 
 ---@param event vim.api.keyset.create_autocmd.callback_args
 local function lsp_attach(event)
