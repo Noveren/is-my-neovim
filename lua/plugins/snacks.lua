792f7df60b60b554b66bb58cca822344c78d1cab
Noveren <1079148276@qq.com>
2026-03-03 22:09:12 +0800

    change: picker.files({ focus = 'list' })

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -57,7 +57,7 @@ return {
         -- Find
         { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
         { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
-        { "<leader>ff", function() Snacks.picker.files({ focus = "input" }) end, desc = "Find Files" },
+        { "<leader>ff", function() Snacks.picker.files({ focus = "list" }) end, desc = "Find Files" },
         { "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
         -- { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
         -- { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
