e49ec4751de82cedf4bb881d7e259e5628409208
noveren <1079148276@qq.com>
2026-03-05 11:01:03 +0800

    dev: 优化 lua_ls 仅为 neovim 配置加载相关类型

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -1,15 +1,22 @@
+---@type string[]
+local library = {}
 
--- TODO 仅在 nvim 配置目录下加入
-local library = {
-    vim.fn.expand("$VIMRUNTIME/lua"),
-    vim.fn.expand("${3rd}/luv/library"),
-}
-local runtimepath = vim.split(vim.api.nvim_get_option_value("runtimepath", {}), ",")
-local lazypath = vim.fn.expand(vim.fn.stdpath("data") .. "/lazy")
-for _, path in ipairs(runtimepath) do
-    local _path = vim.fn.expand(path)
-    if vim.fs.relpath(lazypath, _path) ~= nil then
-        table.insert(library, _path)
+-- TODO 动态加载
+local path_config = vim.fn.expand(vim.fn.stdpath("config"))
+local path_startup_cwd = vim.fn.expand(vim.fn.getcwd())
+if vim.fs.relpath(path_config, path_startup_cwd) ~= nil then
+    vim.notify("Set lua-language-server for neovim configuration.")
+    table.insert(library, vim.fn.expand("$VIMRUNTIME/lua"))
+    table.insert(library, vim.fn.expand("${3rd}/luv/library"))
+
+    local runtimepaths = vim.split(vim.api.nvim_get_option_value("runtimepath", {}), ",")
+    local lazypath = vim.fn.expand(vim.fn.stdpath("data") .. "/lazy")
+    for _, path in ipairs(runtimepaths) do
+        local _path = vim.fn.expand(path)
+        if vim.fs.relpath(lazypath, _path) ~= nil then
+            -- TODO 不添加所有插件
+            table.insert(library, _path)
+        end
     end
 end
 
@@ -17,18 +24,34 @@ end
 return {
     cmd = { "lua-language-server" },
     filetypes = { "lua" },
+    -- FIXME lua_ls 双重加载
+    -- root_markers = {
+    --     "luarc.json",
+    --     "luarc.jsonc",
+    --     ".git",
+    -- },
+    -- before_init = function(client)
+    --     table.insert(library, vim.fn.expand("$VIMRUNTIME/lua"))
+    --     table.insert(library, vim.fn.expand("${3rd}/luv/library"))
+    -- end,
     settings = {
         Lua = {
-            runtime = {
-                version = "LuaJIT",
-            },
+            codeLens = { enable = true },
+            hint = { enable = true, semicolon = "Disable" },
             workspace = {
                 checkThirdParty = false,
                 library = library,
-            },
-            telemetry = {
-                enable = false,
-            },
+            }
+            -- runtime = {
+            --     version = "LuaJIT",
+            -- },
+            -- workspace = {
+            --     checkThirdParty = false,
+            --     library = library,
+            -- },
+            -- telemetry = {
+            --     enable = false,
+            -- },
         },
     }
 }
--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -8,8 +8,22 @@ vim.lsp.enable "ty"     -- python
 -- TODO asm-lsp https://github.com/bergercookie/asm-lsp
 -- TODO https://github.com/typescript-language-server/typescript-language-server
 
----@param event vim.api.keyset.create_autocmd.callback_args
-local function lsp_attach(event)
+-- TODO :Lsp [<language>]
+-- ---@type table<string, string|nil>
+-- local language_server = {
+--     lua = "lua_ls",
+-- }
+-- vim.api.nvim_create_user_command("Lsp", function(args)
+--     vim.notify(args.args)
+-- end, {
+--     nargs = "?",
+--     desc = "Enable/Disable language server.",
+--     force = true,
+-- })
+
+-- TODO checkhealth
+
+local function lsp_attach(_)
     vim.diagnostic.config({
         virtual_text = {
             source = false,
